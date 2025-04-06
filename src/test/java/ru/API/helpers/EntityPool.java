e336eb5070f9b95fd9efd3f33d8e629066322848
benever <thetakky@mail.ru>
2025-04-06 23:16:04 +0300

    EntityPool commit and push

---
+++ src/test/java/ru/API/helpers/EntityPool.java
@@ -0,0 +1,19 @@
+package ru.API.helpers;
+
+import ru.API.pojo.Entity;
+
+public class EntityPool {
+    private static final ThreadLocal<Entity> entity = new ThreadLocal<>();
+    public static Entity getEntity() {
+        return entity.get();
+    }
+    public static void setEntity(Entity newEntity) {
+        entity.set(newEntity);
+    }
+    public static void deleteEntity(){
+        if(entity.get()!=null){
+            BaseRequests.deleteEntityById(entity.get().getId());
+            entity.remove();
+        }
+    }
+}
